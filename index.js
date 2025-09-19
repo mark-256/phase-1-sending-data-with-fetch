a7cd8b6c12930417c14119992b5c2c20da804d9d
Mark <markjohnguto@gmail.com>
2025-09-19 21:53:16 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((response) => response.json())
+    .then((data) => {
+      // Append the ID to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = `User ID: ${data.id}`;
+      body.appendChild(p);
+    })
+    .catch((error) => {
+      // Append the error message to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = `Error: ${error.message}`;
+      body.appendChild(p);
+    });
+}
